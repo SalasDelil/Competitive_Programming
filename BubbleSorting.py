86ddfb23dce2ea98f98fbdab099730b012a4e2b1
SALAS DELIL <98913822+SalasDelil@users.noreply.github.com>
2022-07-13 22:39:39 +0300

    Create BubbleSorting.py

---
+++ BubbleSorting.py
@@ -0,0 +1,13 @@
+def countSwaps(a):
+    num_swaps = 0
+    for i in range(len(a)):
+        for j in range(len(a) - 1):
+            if a[j] > a[j + 1]:
+                tmp = a[j]
+                a[j] = a[j + 1]
+                a[j + 1] = tmp
+                num_swaps += 1
+    print(f"Array is sorted in {num_swaps} swaps.")
+    print(f"First Element: {a[0]}")
+    print(f"Last Element: {a[len(a) - 1]}")
+    
