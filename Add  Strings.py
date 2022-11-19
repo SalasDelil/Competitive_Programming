336fca1f9b3700d50a1c17ab4fb383e4217cf668
SALAS DELIL <98913822+SalasDelil@users.noreply.github.com>
2022-11-19 16:23:55 +0300

    Create Add  Strings.py

---
+++ Add  Strings.py
@@ -0,0 +1,25 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        nums = {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9}
+        k = 1
+        n1 = 0
+        n2 = 0
+        for i in range(len(num1)):
+            if k == 1:
+                n1 += nums[num1[-i - 1]]
+                k *= 10
+            else:
+                n1 += nums[num1[-i - 1]]*k
+                k *= 10
+        t = 1
+        for j in range(len(num2)):
+            if t == 1:
+                n2 += nums[num2[-j - 1]]
+                t *= 10
+            else:
+                n2 += nums[num2[-j - 1]]*t
+                t *= 10
+                
+        result = n1 + n2
+        
+        return str(result)
