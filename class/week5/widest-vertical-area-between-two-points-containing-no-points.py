f89504d2e7dbfb52aa6ebb03a790d432b8f176cf
SALAS DELIL <98913822+SalasDelil@users.noreply.github.com>
2024-01-01 19:34:12 +0300

    Added solution for Widest Vertical Area Between Two Points Containing No Points

---
+++ class/week5/widest-vertical-area-between-two-points-containing-no-points.py
@@ -0,0 +1,5 @@
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda point: point[0])
+        
+        return max(points[i][0]-points[i-1][0] for i in range(1, len(points)))
