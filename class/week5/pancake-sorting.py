8c4011c099e62f74fc874f1f46fe400ec3f69068
SALAS DELIL <98913822+SalasDelil@users.noreply.github.com>
2024-01-01 19:41:15 +0300

    Added solution for Pancake Sorting

---
+++ class/week5/pancake-sorting.py
@@ -0,0 +1,24 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        ans = []
+        n = len(arr)
+
+        for i in range(n):
+            max_ = 0
+            max_ind = 0
+            for j in range(n-i):
+                if arr[j] > max_:
+                    max_ = arr[j]
+                    max_ind = j
+            
+            k = max_ind+1
+            ans.append(k)
+            ans.append(n-i)
+
+            subarr = arr[:k][::-1]
+            arr = subarr + arr[k:]
+
+            subarr  = arr[0:n-i][::-1]
+            arr = subarr + arr[n-i:]
+
+        return ans
