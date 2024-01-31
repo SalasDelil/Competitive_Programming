4a85993ff481d77d597a5c02beac6f61a1d58017
SALAS DELIL <98913822+SalasDelil@users.noreply.github.com>
2024-01-31 22:06:37 +0300

    Added solution for Circular Array Loop

--- class/week9 /circular-array-loop.py
+++ class/week9 /circular-array-loop.py
@@ -4,18 +4,19 @@ class Solution:
 
         for i in range(n):
             if i not in visited:
-                local_s = set()
+                temp_set = set()
 
                 while True:
-                    if i in local_s:
+                    if i in temp_set:
                         return True
                     if i in visited:
                         break
 
                     visited.add(i)
-                    local_s.add(i)
+                    temp_set.add(i)
                     prev, i = i, (i + nums[i]) % n
 
                     if prev == i or (nums[i] > 0) != (nums[prev] > 0):
                         break
+
         return False
