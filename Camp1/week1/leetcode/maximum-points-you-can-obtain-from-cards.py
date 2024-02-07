eeb6be4b35a7918cb6679165a1fcf43dbe9877a2
SALAS DELIL <98913822+SalasDelil@users.noreply.github.com>
2024-02-07 11:48:50 +0300

    Add solution for Maximum Points You Can Obtain from Cards

---
+++ Camp1/week1/leetcode/maximum-points-you-can-obtain-from-cards.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        max_sum = sum(cardPoints[:k])
+        left, right = k-1, len(cardPoints)-1
+        sum_ = max_sum
+
+        while left >= 0:
+            sum_ += (-cardPoints[left] + cardPoints[right])
+            max_sum = max(max_sum, sum_)
+            left -= 1
+            right -= 1
+    
+        return max_sum
