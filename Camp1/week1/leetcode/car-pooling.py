0a6256c98000b9621875edd927430a5cd56bb3bd
SALAS DELIL <98913822+SalasDelil@users.noreply.github.com>
2024-02-08 15:42:57 +0300

    Add solution for Car Pooling

--- Camp1/week1/leetcode/car-pooling.py
+++ Camp1/week1/leetcode/car-pooling.py
@@ -2,13 +2,11 @@ class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         passengers = [0]*(1001)
         max_trip_length = 0
-
+        
         for trip in trips:
             numPassengers = trip[0]
             frm = trip[1]
             to = trip[2]
-            max_trip_length = max(max_trip_length, to)
-
             passengers[frm] += numPassengers
             passengers[to] += -numPassengers
         
