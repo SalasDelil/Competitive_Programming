229c951da6e42784d1506d4321db4f454bc0017a
SALAS DELIL <98913822+SalasDelil@users.noreply.github.com>
2024-02-18 23:34:30 +0300

    Add solution for Rabbits in Forest

--- Camp1/week2/leetcode/rabbits-in-forest.py
+++ Camp1/week2/leetcode/rabbits-in-forest.py
@@ -3,12 +3,12 @@ class Solution:
         counts = Counter(answers)   
         rabbits = 0
 
-        for val in counts:
-            if val == 0:
-                rabbits += counts[val]
-            elif counts[val] <= val + 1:
-                rabbits += val + 1
+        for key in counts:
+            if key == 0:
+                rabbits += counts[key]
+            elif counts[key] <= key + 1:
+                rabbits += key + 1
             else:
-                rabbits +=  (ceil(counts[val]/(val+1)) * (val + 1))
+                rabbits +=  (ceil(counts[key]/(key+1)) * (key + 1))
                 
         return rabbits
