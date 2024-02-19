563c7b39f7ee588cacb612b26a8e5a48b17b29fc
SALAS DELIL <98913822+SalasDelil@users.noreply.github.com>
2024-02-19 14:14:52 +0300

    Add solution for Valid Triangle Number

--- Camp1/week2/leetcode/valid-triangle-number.py
+++ Camp1/week2/leetcode/valid-triangle-number.py
@@ -3,21 +3,17 @@ class Solution:
         n = len(nums)
         if n < 3:
             return 0
-
         nums.sort()
         ans = 0
 
         for i in range(2, len(nums)):
             left, right = 0, i-1
-            count = 0
 
             while left < right:
                 if nums[left]+nums[right] > nums[i]:
-                    count += right-left
+                    ans += right-left
                     right -= 1
                 else:
                     left += 1
-                    
-            ans += count
-                
+            
         return ans
