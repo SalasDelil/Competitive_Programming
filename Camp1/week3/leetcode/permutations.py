a41ececf936a703555905436b18d266811ed63f8
SALAS DELIL <98913822+SalasDelil@users.noreply.github.com>
2024-03-05 16:48:03 +0300

    Add solution for Permutations

---
+++ Camp1/week3/leetcode/permutations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+
+        if len(nums) == 1:
+            return [nums[:]]
+
+        print(path, ans)
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for perm in perms:
+                perm.append(n)
+            ans.extend(perms)
+            nums.append(n)
+
+        return ans
